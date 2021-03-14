4f3cc6638cf56d46c2921144bbd613b359331f75
Andrey Vikulov <avikuloff@yandex.ru>
2021-03-14 14:19:57 +0300

    Рефакторинг

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,23 @@ extern crate gio;
 extern crate gtk;
 
 // To import all needed traits.
-use gdk;
 use gio::prelude::*;
 use gtk::prelude::*;
 
-use gtk::{Box, Button, ButtonBox, ButtonBoxStyle, CheckButton, Label, Orientation, PolicyType, Scale, ScrolledWindow, TextView, NONE_ADJUSTMENT, Entry, SpinButton};
+use gdk;
+use gtk::{Application, ApplicationWindow, Box, Button, ButtonBoxBuilder, ButtonBoxStyle, CheckButton, CheckButtonBuilder, Entry, EntryBuilder, Label, Orientation, PolicyType, Scale, ScrolledWindow, ScrolledWindowBuilder, SpinButton, TextBuffer, TextView, TextViewBuilder, TextBufferBuilder};
 use std::env;
-use upwd_lib::{generate_password, Pool};
-use std::str::FromStr;
+use std::ops::Add;
 use std::rc::Rc;
+use std::str::FromStr;
+use upwd_lib::{generate_password, Pool};
 
-const UPPERCASE: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const LOWERCASE: &'static str = "abcdefghijklmnopqrstuvwxyz";
+const UPPERS: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWERS: &'static str = "abcdefghijklmnopqrstuvwxyz";
 const DIGITS: &'static str = "0123456789";
 const SYMBOLS: &'static str = "*&^%$#@!~";
 
 fn main() {
-    // Todo change app id
     let uiapp = gtk::Application::new(
         Some("com.github.avikuloff.upwd-gtk"),
         gio::ApplicationFlags::FLAGS_NONE,
@@ -26,156 +26,216 @@ fn main() {
     .expect("Application::new failed");
 
     uiapp.connect_activate(|app| {
-        let win = gtk::ApplicationWindow::new(app);
-        win.set_default_size(240, 320);
-        win.set_title("Random Password Generator");
+        // --------------- CREATE MAIN WINDOW --------------- //
+        let win = create_application_window(app);
 
-        let main_box = Box::new(Orientation::Vertical, 0);
+        // --------------- CREATE MAIN CONTAINER --------------- //
+        let main_box = Box::new(Orientation::Vertical, 5);
         main_box.set_property_margin(15);
 
-        let ch_box = Box::new(Orientation::Vertical, 0);
-        let chbox_uppers = CheckButton::with_label("Uppercase letters");
-        let chbox_lowers = CheckButton::with_label("Lowercase letters");
-        let chbox_digits = CheckButton::with_label("Digits");
-        let chbox_symbols = CheckButton::with_label("Special chars");
-        let entry_pool = Rc::new(Entry::new());
-        chbox_uppers.set_active(true);
-        chbox_lowers.set_active(true);
-        chbox_digits.set_active(true);
-        entry_pool.set_text(&format!("{}{}{}", UPPERCASE, LOWERCASE, DIGITS));
-        ch_box.add(&chbox_uppers);
-        ch_box.add(&chbox_lowers);
-        ch_box.add(&chbox_digits);
-        ch_box.add(&chbox_symbols);
-        ch_box.add(&*entry_pool);
-        main_box.add(&ch_box);
+        // --------------- POOL CUSTOMIZATION --------------- //
+        let pool_box = Box::new(Orientation::Vertical, 0);
+        let uppers_chk_btn = create_uppers_chk_btn();
+        let lowers_chk_btn = create_lowers_chk_btn();
+        let digits_chk_btn = create_digits_chk_btn();
+        let symbols_chk_btn = CheckButton::with_label("Special chars");
+        let pool_entry = Rc::new(create_pool_entry());
+        pool_box.add(&uppers_chk_btn);
+        pool_box.add(&lowers_chk_btn);
+        pool_box.add(&digits_chk_btn);
+        pool_box.add(&symbols_chk_btn);
+        pool_box.add(&*pool_entry);
 
+        // --------------- PASSWORD LENGTH --------------- //
         let length_box = Box::new(Orientation::Horizontal, 0);
-        let length_label = Label::new(Some("Length"));
-        let scale_length = Scale::with_range(Orientation::Horizontal, 4.0, 64.0, 1.0);
-        scale_length.set_value(12.0);
+        let (length_label, length_scale) = create_length_scale();
         length_box.pack_start(&length_label, false, false, 0);
-        length_box.pack_end(&scale_length, true, true, 5);
-        main_box.add(&length_box);
+        length_box.pack_end(&length_scale, true, true, 5);
 
+        // --------------- NUMBER OF PASSWORDS --------------- //
         let num_password_box = Box::new(Orientation::Vertical, 0);
-        num_password_box.set_margin_top(10);
-        num_password_box.set_margin_bottom(10);
-        let num_password_label = Label::new(Some("Number of passwords"));
-        let num_password_spin_btn = SpinButton::with_range(1_f64, 100_f64, 1_f64);
-        num_password_spin_btn.set_value(1.0);
+        let (num_password_label, num_password_spin_btn) = create_num_passwords_spin_btn();
         num_password_box.add(&num_password_label);
         num_password_box.add(&num_password_spin_btn);
-        main_box.add(&num_password_box);
 
-        let bbox = ButtonBox::new(Orientation::Horizontal);
-        bbox.set_layout(ButtonBoxStyle::Expand);
+        // --------------- CREATE SHOW PASSWORDS WINDOW --------------- //
+        let scrolled_window = create_scrolled_window();
+        let passwords_box = Box::new(Orientation::Vertical, 0);
+        let (passwords_text_view, passwords_text_buffer) = create_passwords_text_view();
+        scrolled_window.add(&passwords_text_view);
+        passwords_box.pack_start(&scrolled_window, true, true, 0);
+
+        // --------------- GENERATE AND COPY PASSWORDS BUTTONS --------------- //
+        let btn_box = ButtonBoxBuilder::new()
+            .orientation(Orientation::Horizontal)
+            .layout_style(ButtonBoxStyle::Expand)
+            .build();
         let btn_generate = Rc::new(Button::with_label("Generate"));
         let btn_copy = Button::with_label("Copy");
-        bbox.add(&*btn_generate);
-        bbox.add(&btn_copy);
-        let text = TextView::new();
-        let buf = text.get_buffer().unwrap();
-        buf.set_text("");
+        btn_box.add(&*btn_generate);
+        btn_box.add(&btn_copy);
 
-        let sw = ScrolledWindow::new(NONE_ADJUSTMENT, NONE_ADJUSTMENT);
-        sw.set_min_content_height(80);
-        sw.set_policy(PolicyType::Automatic, PolicyType::Automatic);
-        sw.add(&text);
+        // --------------- POPULATE MAIN CONTAINER --------------- //
+        main_box.add(&pool_box);
+        main_box.add(&length_box);
+        main_box.add(&num_password_box);
+        main_box.pack_start(&passwords_box, true, true, 0);
+        main_box.pack_end(&btn_box, false, false, 0);
 
-        let text_box = Box::new(Orientation::Vertical, 5);
-        text_box.pack_start(&sw, false, false, 0);
-        text_box.pack_start(&bbox, false, false, 0);
-        main_box.add(&text_box);
-
+        // --------------- POPULATE MAIN WINDOW --------------- //
         win.add(&main_box);
         win.show_all();
 
-        let clone_entry_pool = entry_pool.clone();
-        chbox_uppers.connect_toggled(move |v| {
-            let mut pool = Pool::from_str(&clone_entry_pool.get_text()).unwrap();
-
-            if v.get_active() {
-                pool.extend_from_string(UPPERCASE);
-            } else {
-                pool.remove_all(UPPERCASE);
-            }
-
-            &clone_entry_pool.set_text(&pool.to_string());
+        // --------------- HANDLE SIGNALS --------------- //
+        let clone_entry_pool = pool_entry.clone();
+        uppers_chk_btn.connect_toggled(move |chk_btn| {
+            handle_chk_btn_toggled(chk_btn, &*clone_entry_pool, UPPERS)
         });
-        let clone_entry_pool = entry_pool.clone();
-        chbox_lowers.connect_toggled(move |v| {
-            let mut pool = Pool::from_str(&clone_entry_pool.get_text()).unwrap();
-
-            if v.get_active() {
-                pool.extend_from_string(LOWERCASE);
-            } else {
-                pool.remove_all(LOWERCASE);
-            }
-
-            &clone_entry_pool.set_text(&pool.to_string());
+        let clone_entry_pool = pool_entry.clone();
+        lowers_chk_btn.connect_toggled(move |chk_btn| {
+            handle_chk_btn_toggled(chk_btn, &*clone_entry_pool, LOWERS)
         });
-        let clone_entry_pool = entry_pool.clone();
-        chbox_digits.connect_toggled(move |v| {
-            let mut pool = Pool::from_str(&clone_entry_pool.get_text()).unwrap();
-
-            if v.get_active() {
-                pool.extend_from_string(DIGITS);
-            } else {
-                pool.remove_all(DIGITS);
-            }
-
-            &clone_entry_pool.set_text(&pool.to_string());
+        let clone_entry_pool = pool_entry.clone();
+        digits_chk_btn.connect_toggled(move |chk_btn| {
+            handle_chk_btn_toggled(chk_btn, &*clone_entry_pool, DIGITS)
         });
-        let clone_entry_pool = entry_pool.clone();
-        chbox_symbols.connect_toggled(move |v| {
-            let mut pool = Pool::from_str(&clone_entry_pool.get_text()).unwrap();
-
-            if v.get_active() {
-                pool.extend_from_string(SYMBOLS);
-            } else {
-                pool.remove_all(SYMBOLS);
-            }
-
-            &clone_entry_pool.set_text(&pool.to_string());
+        let clone_entry_pool = pool_entry.clone();
+        symbols_chk_btn.connect_toggled(move |chk_btn| {
+            handle_chk_btn_toggled(chk_btn, &*clone_entry_pool, SYMBOLS)
         });
 
-        let clone_entry_pool = entry_pool.clone();
         let clone_btn_generate = btn_generate.clone();
-        clone_entry_pool.connect_changed(move |v| {
-            if v.get_text_length() == 0 {
-                clone_btn_generate.set_sensitive(false);
-                return;
-            } else {
-                clone_btn_generate.set_sensitive(true);
-            }
+        pool_entry
+            .connect_changed(move |entry| handle_pool_entry_changed(entry, &*clone_btn_generate));
+
+        btn_generate.connect_clicked(move |_btn| {
+            handle_generate_btn_clicked(
+                &pool_entry.get_text(),
+                length_scale.get_value() as usize,
+                num_password_spin_btn.get_value() as u8,
+                &passwords_text_buffer,
+            )
         });
 
-        btn_generate.connect_clicked(move |_| {
-            let len = scale_length.get_value() as usize;
-            let num_passwords = num_password_spin_btn.get_value() as u8;
-            let pool = Pool::from_str(&entry_pool.get_text()).unwrap();
-
-            for i in 0..num_passwords {
-                let mut password = generate_password(&pool, len);
-                if i == 0 {
-                    buf.set_text(&password);
-                } else {
-                    password.insert(0, '\n');
-                    buf.insert(&mut buf.get_iter_at_line(i as i32), &password)
-                }
-            }
-        });
-
-        // Todo add tooltip
-        btn_copy.connect_clicked(move |_btn| {
-            let clipboard = &text.get_clipboard(&gdk::SELECTION_CLIPBOARD);
-            let buffer = &text.get_buffer().unwrap();
-            let text = buffer
-                .get_text(&buffer.get_start_iter(), &buffer.get_end_iter(), false)
-                .unwrap();
-            clipboard.set_text(&text);
-        });
+        btn_copy.connect_clicked(move |_btn| handle_copy_btn_clicked(&passwords_text_view));
+        // --------------- END HANDLE SIGNALS --------------- //
     });
     uiapp.run(&env::args().collect::<Vec<_>>());
+}
+
+fn create_application_window(app: &Application) -> ApplicationWindow {
+    gtk::ApplicationWindowBuilder::new()
+        .application(app)
+        .default_width(255)
+        .default_height(360)
+        .title("Random Password Generator")
+        .build()
+}
+
+fn create_uppers_chk_btn() -> CheckButton {
+    CheckButtonBuilder::new()
+        .label("Uppercase letters")
+        .active(true)
+        .build()
+}
+
+fn create_lowers_chk_btn() -> CheckButton {
+    CheckButtonBuilder::new()
+        .label("Lowercase letters")
+        .active(true)
+        .build()
+}
+
+fn create_digits_chk_btn() -> CheckButton {
+    CheckButtonBuilder::new()
+        .label("Digits")
+        .active(true)
+        .build()
+}
+
+fn create_pool_entry() -> Entry {
+    EntryBuilder::new()
+        .text(&String::from(UPPERS).add(LOWERS).add(DIGITS))
+        .build()
+}
+
+fn create_length_scale() -> (Label, Scale) {
+    let length_scale = Scale::with_range(Orientation::Horizontal, 4.0, 64.0, 1.0);
+    length_scale.set_value(12.0);
+
+    (Label::new(Some("Length")), length_scale)
+}
+
+fn create_num_passwords_spin_btn() -> (Label, SpinButton) {
+    let num_password_spin_btn = SpinButton::with_range(1.0, 100.0, 1.0);
+    num_password_spin_btn.set_value(1.0);
+
+    (
+        Label::new(Some("Number of passwords")),
+        num_password_spin_btn,
+    )
+}
+
+fn create_passwords_text_view() -> (TextView, TextBuffer) {
+    let buffer = TextBufferBuilder::new().text("").build();
+    let view = TextViewBuilder::new().buffer(&buffer).border_width(3).build();
+
+    (view, buffer)
+}
+
+fn create_scrolled_window() -> ScrolledWindow {
+    ScrolledWindowBuilder::new()
+        .min_content_height(65)
+        .vscrollbar_policy(PolicyType::Always)
+        .build()
+}
+
+// В зависимости от состояния `chk_btn` добавляет или удаляет символы `string` из `entry`
+fn handle_chk_btn_toggled(chk_btn: &CheckButton, entry: &Entry, string: &str) {
+    let mut pool = Pool::from_str(&entry.get_text()).unwrap();
+
+    if chk_btn.get_active() {
+        pool.extend_from_string(string);
+    } else {
+        pool.remove_all(string);
+    }
+
+    entry.set_text(&pool.to_string());
+}
+
+// Если `entry` не содержит ни одного символа, то кнопка `btn_generate` блокируется
+fn handle_pool_entry_changed(entry: &Entry, btn_generate: &Button) {
+    if entry.get_text_length() == 0 {
+        btn_generate.set_sensitive(false);
+    } else {
+        btn_generate.set_sensitive(true);
+    }
+}
+
+// Создает `num_passwords` паролей длиной `length` символов, используя символы определенные в `pool`.
+// Перезаписывает `buffer`
+fn handle_generate_btn_clicked(pool: &str, length: usize, num_passwords: u8, buffer: &TextBuffer) {
+    let pool = Pool::from_str(pool).unwrap();
+
+    for i in 0..num_passwords {
+        let password = generate_password(&pool, length);
+        if i == 0 {
+            buffer.set_text(&password);
+        } else {
+            let iter = &mut buffer.get_iter_at_line(i as i32);
+            buffer.insert(iter, "\n");
+            buffer.insert(iter, &password);
+        }
+    }
+}
+
+// Копирует пароли из `text_view` в буфер обмена
+fn handle_copy_btn_clicked(text_view: &TextView) {
+    let clipboard = text_view.get_clipboard(&gdk::SELECTION_CLIPBOARD);
+    let buffer = text_view.get_buffer().unwrap();
+    let text = buffer
+        .get_text(&buffer.get_start_iter(), &buffer.get_end_iter(), false)
+        .unwrap();
+
+    clipboard.set_text(&text);
 }
